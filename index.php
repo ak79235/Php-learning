a3ae0a98ccdb876154d56a177623eae25a2de84b
ak79235 <ak79235@gmail.com>
2022-02-02 19:40:19 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP tutorial</title>
+</head>
+<body>
+    <div class="container">
+        First php website
+        <?php 
+        echo "<br>Print command in PHP"; 
+        echo "<br>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Minus totam voluptas voluptatem at. Consequuntur dignissimos ratione repudiandae 
+        optio unde alias error facilis nulla nisi, ut illum autem praesentium rerum non."
+        ?>
+        
+    </div>
+    </body>
+</html>
