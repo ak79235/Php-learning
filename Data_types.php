af4c48b552c9a4c84bd187605ede63423c77a829
ak79235 <ak79235@gmail.com>
2022-02-02 23:14:46 +0530

    Data types

---
+++ Data_types.php
@@ -0,0 +1,22 @@
+<?php
+    //Differnt data types in php
+    // String 
+    // Integer
+    // Float
+    // Boolean
+    // Array
+    // Object
+    echo "Data types:<br>";
+    $var="This is a string";
+    echo var_dump($var);
+    echo "<br>";
+    $var=35;
+    echo var_dump($var);
+    echo "<br>";
+    $var= 3.14;
+    echo var_dump($var);
+    echo "<br>";
+    $var= TRUE;
+    echo var_dump($var);
+    echo "<br>";
+?>
