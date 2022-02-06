cc3e33ac7f5d403f914b5cd427cd1ef92e912299
ak79235 <ak79235@gmail.com>
2022-02-06 17:00:01 +0530

    Loops in basic site

--- Basic_site.php
+++ Basic_site.php
@@ -37,6 +37,30 @@
             margin: auto;
             padding: 25px;
         }
+        .AIL{
+            max-width:80%;
+            background-color: antiquewhite;
+            margin: auto;
+            padding: 25px;
+        }
+        .do-while{
+            max-width:80%;
+            background-color: antiquewhite;
+            margin: auto;
+            padding: 25px;
+        }
+        .for{
+            max-width:80%;
+            background-color: antiquewhite;
+            margin: auto;
+            padding: 25px;
+        }
+        .for-each{
+            max-width:80%;
+            background-color: antiquewhite;
+            margin: auto;
+            padding: 25px;
+        }
         .h1{
             text-align: center;
         }
@@ -47,7 +71,7 @@
         <h1 >Let's learn about PHP</h1>
     </div>
     <div class="if">
-    <p><br> status:</p>
+    <p><br><b> Status:</b></p>
         <?php
             $age=19;
             if($age>18){
@@ -62,9 +86,9 @@
         <h3>Arrays in PHP</h3>
         <?php
             $numbers=array(6,7,8,9,10);
-            echo "<br>";
+            echo "<br>0th element of array:";
             echo $numbers[0];
-            echo "<br>";
+            echo "<br>No of elements in array:";
             echo count($numbers);
         ?>
     </div>
@@ -76,6 +100,49 @@
                 echo "<br>The value of a is:";
                 echo $a;
                 $a++;
+            }
+        ?>
+    </div>
+    <div class="AIL">
+        <h3>Iterating arrays with the help of loops</h3>
+        <?php
+            $a=0;
+            while($a<count($numbers)){
+                echo "<br>Value of array at [";
+                echo $a;
+                echo "] is : ";
+                echo $numbers[$a];
+                $a++;
+            }
+        ?>
+    </div>
+    <div class="do-while">
+        <h3>Example of itereating loops using do-while</h3>
+        <?php
+            $a=0;
+            do{
+                echo "<br>Value of a is:";
+                echo $a;
+                $a++;
+            }while($a<=10);
+        ?>
+    </div>
+    <div class="for">
+        <h3>Example of itereating loops using for loop</h3>
+        <?php
+            for ($a=0; $a < 10 ; $a++) { 
+                echo "<br>Value of a is:";
+                echo $a;
+            }
+        ?>
+    </div>
+    <div class="for-each">
+        <h3>Example of itereating loops using for-each</h3>
+        <?php
+            $languages=array("Python","Java","CPP");
+            foreach ($languages as  $value) {
+                echo "<br>Printing array with for-each loop:";
+                echo $value;
             }
         ?>
     </div>
